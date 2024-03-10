f5954dbd548664543446ca3c38957f222f481d06
Alexander Ilinykh <divinenickname@gmail.com>
2024-03-10 17:18:22 +0500

    'Reload From Disk' action after generating tests

--- src/main/kotlin/io.github.divinenickname.kotlin.utgen.idea.plugin/GenerateUnitTest.kt
+++ src/main/kotlin/io.github.divinenickname.kotlin.utgen.idea.plugin/GenerateUnitTest.kt
@@ -3,6 +3,9 @@ package io.github.divinenickname.kotlin.utgen.idea.plugin
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.project.guessProjectDir
+import com.intellij.openapi.vfs.VirtualFileManager
 import io.github.divinenickname.kotlin.utgen.core.UnitTestGenerator
 import java.io.File
 
@@ -13,5 +16,13 @@ class GenerateUnitTest : AnAction() {
             .apply {
                 UnitTestGenerator().generateAndSave(this.toPath())
             }
+            .also {
+                ApplicationManager.getApplication().invokeLater {
+                    VirtualFileManager.getInstance().syncRefresh()
+                    e.project?.guessProjectDir()?.refresh(false, true)
+                        ?: throw RuntimeException("Project dir not found. Try to 'Reload From Disk'")
+                }
+            }
+
     }
 }
