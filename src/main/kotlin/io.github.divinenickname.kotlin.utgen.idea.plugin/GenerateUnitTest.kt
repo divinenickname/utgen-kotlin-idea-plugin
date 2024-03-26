80e5d02456e148463499343a949d01172909f699
Alexander Ilinykh <divinenickname@gmail.com>
2024-03-26 18:53:57 +0500

    refactor PSI file generation for support 2022.3.3 IDE

--- src/main/kotlin/io.github.divinenickname.kotlin.utgen.idea.plugin/GenerateUnitTest.kt
+++ src/main/kotlin/io.github.divinenickname.kotlin.utgen.idea.plugin/GenerateUnitTest.kt
@@ -4,12 +4,19 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.project.guessProjectDir
-import com.intellij.openapi.vfs.findOrCreateDirectory
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VfsUtil
+import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiFileFactory
-import com.intellij.psi.impl.file.PsiDirectoryFactory
+import com.intellij.psi.PsiManager
+import com.intellij.psi.codeStyle.CodeStyleManager
+import com.squareup.kotlinpoet.FileSpec
 import io.github.divinenickname.kotlin.utgen.core.UnitTestGenerator
 import org.jetbrains.kotlin.idea.KotlinFileType
+import org.jetbrains.kotlin.lexer.KtTokens
+import org.jetbrains.kotlin.psi.KtClassOrObject
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtFunction
 import java.io.File
 
 class GenerateUnitTest : AnAction() {
@@ -18,22 +25,34 @@ class GenerateUnitTest : AnAction() {
         val project = e.project ?: throw RuntimeException("Project must not be null. Critical failure.")
         val code = e.getData(CommonDataKeys.VIRTUAL_FILE)!!.path.let(::File).toPath()
             .let { UnitTestGenerator().generate(it) }
+        val psiFile = makePsiFile(project, code)
 
-        val packageToPath = "src/test/kotlin/" + code.relativePath.removeSuffix("${code.name}.kt")
-        val baseDir = project.guessProjectDir()!!.findOrCreateDirectory(packageToPath)
-        val fileName = "${code.name}.kt"
+        val basePath = project.basePath!!
+            .plus("/src/test/kotlin/")
+            .plus(code.relativePath).removeSuffix("/${code.name}.kt")
 
-        val psiDirectory = PsiDirectoryFactory.getInstance(project).createDirectory(baseDir)
+        val directory = VfsUtil.createDirectoryIfMissing(basePath)!!
+        PsiManager.getInstance(project).findDirectory(directory)!!
+            .takeIf { it.findFile(psiFile.name) == null }
+            ?.run {
+                ApplicationManager.getApplication().runWriteAction { this.add(psiFile) }
+            }
+    }
 
-        ApplicationManager.getApplication().runWriteAction { psiDirectory.findFile(fileName)?.delete() }
+    private fun makePsiFile(project: Project, fileSpec: FileSpec): PsiFile {
+        val fileName = "${fileSpec.name}.kt"
+        val psiFile =  PsiFileFactory.getInstance(project)
+            .createFileFromText(fileName, KotlinFileType.INSTANCE, fileSpec.toString())
 
-        val psiFile = PsiFileFactory.getInstance(project)
-            .createFileFromText(fileName, KotlinFileType.INSTANCE, code.toString())
-
-        CodeFormatter(project, psiFile).apply()
-
-        ApplicationManager.getApplication().runWriteAction {
-            psiDirectory.add(psiFile)
+        (psiFile as KtFile).declarations.forEach { declaration ->
+            if (declaration is KtClassOrObject) {
+                declaration.declarations.filterIsInstance<KtFunction>()
+                    .filter { it.hasModifier(KtTokens.PUBLIC_KEYWORD) }
+                    .forEach { it.removeModifier(KtTokens.PUBLIC_KEYWORD) }
+            }
         }
+        CodeStyleManager.getInstance(project).reformat(psiFile)
+
+        return psiFile
     }
 }
